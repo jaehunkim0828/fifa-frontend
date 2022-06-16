8f6bb55a4885e0a1d71d557cdf084a3ace97f9cf
JaeHun Kim <kkaa81@naver.com>
2022-06-16 23:58:46 +0900

    nav component

---
+++ components/Nav.tsx
@@ -0,0 +1,11 @@
+import style from '../styles/nav.module.scss';
+
+export default function Nav() {
+    return (
+        <div className={style.navContainer}>
+            
+            <button>선수 조회</button>
+            <button>유저 조회</button>
+        </div>
+    )
+};
