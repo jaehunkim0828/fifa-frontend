1bd1e3a1a2a7f373e22f2a8989904538c8104760
JaeHun Kim <kkaa81@naver.com>
2022-06-17 00:01:32 +0900

    add nav component in every pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Nav from '../components/Nav';
+import '../styles/globals.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='main'>
+      <Nav />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
