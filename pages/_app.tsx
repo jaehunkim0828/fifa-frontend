b36ab13f927ef79e3e5ee6fffa4e4b0160f7dd4a
JaeHun Kim <kkaa81@naver.com>
2022-07-15 01:54:10 +0900

    LSint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-import Nav from '../components/Nav';
-import '../styles/globals.css';
+import Nav from "../components/Nav";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className='main'>
+    <div className="main">
       <Nav />
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <body>
+          <Head></Head>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+};
