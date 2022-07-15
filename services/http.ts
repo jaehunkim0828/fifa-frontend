b74f308485c22ffdfa268d515847ed7be8c4ee54
JaeHun Kim <kkaa81@naver.com>
2022-07-15 23:59:23 +0900

    change ip

--- services/http.ts
+++ services/http.ts
@@ -1,22 +1,22 @@
 import axios from "axios";
 
-const url = 'http://localhost:8080';
+const url = "http://3.37.55.226:8080";
 
 export const getMethod = async (path: string, params?: object) => {
-    const data = await axios.get(`${url}/${path}`, {
-        params: {...params},
-    });
+  const data = await axios.get(`${url}/${path}`, {
+    params: { ...params },
+  });
 
-    return data.data;
-}
+  return data.data;
+};
 
-export const postMethod = async(path: string, body: object): Promise<any> => {
-    let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
-        }
-      };
-    const data = await axios.post(`${url}/${path}`, body, axiosConfig);
-    return data;
-}
+export const postMethod = async (path: string, body: object): Promise<any> => {
+  let axiosConfig = {
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+  const data = await axios.post(`${url}/${path}`, body, axiosConfig);
+  return data;
+};
