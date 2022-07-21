27b7e1106c81565952f0f8cf140d88c14c0b874e
JaeHun Kim <kkaa81@naver.com>
2022-07-22 00:57:26 +0900

    add rank

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,33 @@
-import type { NextPage } from "next";
-
-import style from "../styles/Home.module.scss";
-import Player from "../components/Player";
 import Seo from "@components/Seo";
+import { getMethod } from "@services/http";
+import Rank from "@components/rank/Rank";
+import { HomeProps } from "@type/Home.type";
 
-const Home = ({ result }: { result: string }) => {
-  console.log(result);
+export default function Home({ playerRanks, totalCount, count }: HomeProps) {
   return (
     <>
       <Seo title={"Home"} />
-      <div className={style.homeContainer}>
-        <Player />
-      </div>
+      <Rank playerRanks={playerRanks} totalCount={totalCount} count={count} />
     </>
   );
-};
-
-export default Home;
+}
 
 export async function getServerSideProps() {
   // 여기서 axios를 사용해주면 개꿀
+  const count = 12;
+  const getFirstPage = async () => {
+    return await getMethod(`rank/all?current_page=1&count=${count}`);
+  };
 
-  const result = "hello";
+  const countTotalRankPlayer = async () => {
+    return await getMethod("rank/player/count");
+  };
+
   return {
     props: {
-      result,
+      playerRanks: await getFirstPage(),
+      totalCount: await countTotalRankPlayer(),
+      count,
     },
   };
 }
