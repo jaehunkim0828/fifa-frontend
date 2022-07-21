e2cbcc47c9dcecf104ca4fed61122a007df5604e
JaeHun Kim <kkaa81@naver.com>
2022-07-22 00:57:18 +0900

    add rank component

---
+++ components/rank/Rank.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import useThumb from "@hooks/useThumb";
+import style from "./rank.module.scss";
+import PlayerThumb from "@components/PlayerThumb";
+import Pagination from "@components/Pagination";
+import RankService from "@services/rank.api";
+import { PlayerRank } from "@type/Home.type";
+
+export default function Rank({ playerRanks, totalCount, count }: any) {
+  const rankService = new RankService();
+
+  const [comparedThumb, setComparedThumb] = useThumb([]);
+  const [ranks, setRanks] = useState<PlayerRank[]>(playerRanks);
+
+  const getCurrentPage = async (cur_page: number) => {
+    let c = count;
+    const ranksData: PlayerRank[] = await rankService.getCurrentPage(
+      cur_page,
+      c
+    );
+    setRanks(ranksData);
+  };
+
+  return (
+    <div className={style.homeContainer}>
+      <div className={style.playerRanks}>
+        {ranks.map(
+          ({ id, name, season: { className, seasonImg } }: PlayerRank, i) => {
+            return (
+              <PlayerThumb
+                key={i}
+                spid={id}
+                comparedThumb={comparedThumb}
+                setComparedThumb={setComparedThumb}
+                name={name}
+                seasonImg={seasonImg}
+                classname={className}
+              />
+            );
+          }
+        )}
+      </div>
+      <Pagination
+        totalCount={totalCount}
+        count={count}
+        getCurrentPage={getCurrentPage}
+      />
+    </div>
+  );
+}
