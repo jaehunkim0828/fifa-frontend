2671fccc74c23f98c71da1952d08831456a8045b
JaeHun Kim <kkaa81@naver.com>
2022-07-31 18:18:08 +0900

    주석 지워줌

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,6 @@ export default function Home({ playerRanks, totalCount, count }: HomeProps) {
 }
 
 export async function getServerSideProps() {
-  // 여기서 axios를 사용해주면 개꿀
   const count = 9;
   const getFirstPage = async () => {
     return await getMethod(`rank/all?current_page=1&count=${count}`);
