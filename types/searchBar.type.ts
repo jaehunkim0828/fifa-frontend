7269f11c566c4f9467e1da04d1f9138fba289efc
JaeHun Kim <kkaa81@naver.com>
2022-08-22 20:55:09 +0900

    addsetplayer type

--- types/searchBar.type.ts
+++ types/searchBar.type.ts
@@ -2,4 +2,5 @@ export interface SearchBarProps {
   player: string;
   onChangePlayer: (event: React.ChangeEvent<HTMLInputElement>) => void;
   submit: (e: React.SyntheticEvent) => void;
+  setPlayer: (value: string) => void;
 }
