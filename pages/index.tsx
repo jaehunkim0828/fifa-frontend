6aa6d3794affbb39bd848359c650cbccf1f6e6d9
JaeHun Kim <kkaa81@naver.com>
2022-08-25 00:03:48 +0900

    remove console

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Seo from "@components/rest/Seo";
 import StartSearch from "@components/startSearch/StartSearch";
 
 const Search = () => {
-  console.log(process.env.NODE_ENV);
   return (
     <>
       <Seo title={"Search"} />
