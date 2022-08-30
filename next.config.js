3f4aa481caba33676de0bfc88404accc2af3f1dc
JaeHun Kim <kkaa81@naver.com>
2022-08-30 21:39:44 +0900

    fix ip

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["ssl.nexon.com", "3.37.55.226", "fo4.dn.nexoncdn.co.kr"],
+    domains: ["ssl.nexon.com", "43.200.109.133", "fo4.dn.nexoncdn.co.kr"],
   },
   webpack(config) {
     config.resolve.modules.push(__dirname); // 추가
