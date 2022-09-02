eaf97a1345d6576a35976cbf33b61a8cf862f5cc
JaeHun Kim <kkaa81@naver.com>
2022-09-03 00:52:29 +0900

    add player api

--- services/player.api.ts
+++ services/player.api.ts
@@ -1,4 +1,5 @@
-import { getMethod } from "./http";
+import { PositionPart } from "@type/playerThumb.type";
+import { getMethod, postMethod } from "./http";
 
 export default class PlayerService {
   public async getPlayersByName(
@@ -11,5 +12,13 @@ export default class PlayerService {
         player
       )}?current_page=${current_page}&count=${count}`
     );
+  }
+
+  public async getMyTotalRankByPo(spid: string, value: number) {
+    return getMethod(`rank/potential?spid=${spid}&po=${value}`);
+  }
+
+  public async getAverageStates(part: PositionPart) {
+    return getMethod(`position/average/${part}`);
   }
 }
