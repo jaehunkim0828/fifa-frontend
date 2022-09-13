12507950c4102e13c4931ea8bbc91699fecdc191
JaeHun Kim <kkaa81@naver.com>
2022-09-13 11:56:08 +0900

    value 소문자로 바꿔주기

--- helpers/image.ts
+++ helpers/image.ts
@@ -2,4 +2,6 @@
  * @argument value - 사진 이름 ex) mf.png value = mf
  */
 export const publicImage = (value: string) =>
-  `https://${process.env.NEXT_PUBLIC_FRONT_DOMAIN}/images/${value}.png`;
+  `http://${
+    process.env.NEXT_PUBLIC_FRONT_DOMAIN
+  }/images/${value.toLowerCase()}.png`;
