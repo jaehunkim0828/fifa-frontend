ae6bfcd30489019e22ee817572a3ad596ceab4a2
JaeHun Kim <kkaa81@naver.com>
2022-10-15 01:02:10 +0900

    update mail

--- components/detail/Detail.tsx
+++ components/detail/Detail.tsx
@@ -3,23 +3,33 @@ import Select from "react-select";
 
 import style from "./detail.module.scss";
 import { DetailProps } from "@type/detail.type";
+import { useState } from "react";
+import Randar from "@components/radar/Radar";
 
 export default function Detail({
   seleteOptions,
   showPlayerGraph,
   stats,
+  isGraph,
+  setisGraph,
 }: DetailProps) {
   return (
     <div className={style.playerContainer}>
       <div className={style.graph}>
         <div className={style.select}>
+          <button onClick={() => setisGraph(true)}>Bar</button>
+          <button onClick={() => setisGraph(false)}>Radar</button>
           <Select
             defaultValue={seleteOptions[0]}
             options={seleteOptions}
             onChange={(e: any) => showPlayerGraph(e.value)}
           />
         </div>
-        <Graph stats={stats} seasonImg={"1"} />
+        {isGraph ? (
+          <Graph stats={stats} seasonImg={"1"} />
+        ) : (
+          <Randar stats={stats} />
+        )}
       </div>
     </div>
   );
